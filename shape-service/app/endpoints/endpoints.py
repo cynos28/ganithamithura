1693d1f5e06838085713776f609c59569f8fb5ea
Dilshan Kumara <140689829+Dilshankumara0122@users.noreply.github.com>
2025-11-07 22:36:48 +0500

    feat: integrate AI model for predict shapes using real world images

--- shape-service/app/endpoints/endpoints.py
+++ shape-service/app/endpoints/endpoints.py
@@ -1,4 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File, Request
+from app.controllers.shapes_detection import ShapesDetectionController
+
 
 
 router = APIRouter()
+
+shapes_detection_controller = ShapesDetectionController()
+
+
+@router.post("/detect-shape/")
+async def detect_shape(request: Request, image_file: UploadFile = File(None)):
+    return await shapes_detection_controller.detect_shape(request, image_file)
---
+++ shape-service/app/services/shape_predict.py
@@ -0,0 +1,33 @@
+import numpy as np
+from PIL import Image
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+import requests
+from io import BytesIO
+
+def get_shape_from_image(image_input):
+    labels = [
+        "None",
+        "Circle",
+        "Triangle",
+        "Square",
+        "Pentagon",
+        "Hexagon"
+    ]
+    
+    if isinstance(image_input, str):
+        # Assume it's a URL
+        image = Image.open(requests.get(image_input, stream=True).raw)
+    else:
+        # Assume it's a file-like object
+        image = Image.open(image_input)
+    
+    feature_extractor = AutoImageProcessor.from_pretrained('0-ma/mobilenet-v2-geometric-shapes')
+    model = AutoModelForImageClassification.from_pretrained('0-ma/mobilenet-v2-geometric-shapes')
+    
+    inputs = feature_extractor(images=image, return_tensors="pt")
+    logits = model(**inputs)['logits'].cpu().detach().numpy()
+    
+    prediction = np.argmax(logits, axis=1)[0]
+    predicted_label = labels[prediction]
+    
+    return predicted_label
