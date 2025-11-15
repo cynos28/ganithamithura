225b05fe856fb1e4b34f9dc4750bb94693d20f77
Dilshan Kumara <140689829+Dilshankumara0122@users.noreply.github.com>
2025-11-15 21:47:58 +0500

    refactor: add changes to shapes APIs

--- shape-service/app/controllers/shapes_controller.py
+++ shape-service/app/controllers/shapes_controller.py
@@ -6,43 +6,39 @@ class ShapesController:
         self.db = get_database()
     
     
-    """
-    Retrieves all shapes from the database.
-
-    Returns:
-        list: A list of dictionaries, where each dictionary represents a shape.
-
-    Raises:
-        HTTPException: If no shapes are found in the database.
-    """
     async def get_shapes(self):
+        """
+        Retrieves all shapes from the database.
+
+        Returns:
+            list: A list of dictionaries, where each dictionary represents a shape.
+
+        Raises:
+            HTTPException: If no shapes are found in the database.
+        """
         shapes = []
-        pipeline = [
-            {
-                "$match": {}
-            }
-        ]
-        cursor = self.db.shapes.aggregate(pipeline)
-        if cursor:
-            async for document in cursor:
-                document['_id'] = str(document['_id'])
-                shapes.append(document)
-            return shapes
-        raise HTTPException(status_code=404, detail="Shape not found")
+        cursor = self.db.shapes.find({})
+        async for document in cursor:
+            document['_id'] = str(document['_id'])
+            shapes.append(document)
+        
+        if not shapes:
+            raise HTTPException(status_code=404, detail="No shapes found")
+        return shapes
     
-    """
-    Retrieves a single shape by its ID.
-
-    Args:
-        image_id (str): The ID of the image to retrieve.
-
-    Returns:
-        dict: A dictionary containing the shape's data (id, name, description, image_url).
-
-    Raises:
-        HTTPException: If no shape with the given ID is found in the database.
-    """
     async def get_image_by_id(self, image_id: str):
+        """
+        Retrieves a single shape by its ID.
+
+        Args:
+            image_id (str): The ID of the image to retrieve.
+
+        Returns:
+            dict: A dictionary containing the shape's data (id, name, description, image_url).
+
+        Raises:
+            HTTPException: If no shape with the given ID is found in the database.
+        """
         shape = await self.db.shapes.find_one({"id": image_id})
         if shape:
             data = {
