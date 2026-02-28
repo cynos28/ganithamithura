1674e6d1ca873e10e908061ac6b71afa842b1ec4
Nethmi <99679540+NethmiJayarathna@users.noreply.github.com>
2026-03-01 00:08:06 +0530

    fixed the issue.

--- auth_service/database.py
+++ auth_service/database.py
@@ -1,13 +1,14 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
+import certifi
 
 load_dotenv()
 
 # Connect to the MongoDB cluster provided by the user
 MONGODB_URL = os.getenv("MONGODB_URL", "mongodb+srv://nethmi:nethmi@itpm.nbiremo.mongodb.net/")
 
-client = AsyncIOMotorClient(MONGODB_URL)
+client = AsyncIOMotorClient(MONGODB_URL, tlsCAFile=certifi.where())
 database = client["ganithamithura"]
 users_collection = database["users"]
 
